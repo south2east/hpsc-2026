deac2736f73e78a1c631f4acb3077257c4dee1a3
south2east <0603kendohm@gmail.com>
2026-05-19 21:46:42 +0900

    submit 16, 17

--- 09_io/17_phdf5_read.cpp
+++ 09_io/17_phdf5_read.cpp
@@ -6,12 +6,13 @@
 using namespace std;
 
 int main (int argc, char** argv) {
-  hsize_t dim[2] = {2, 2};
+  hsize_t dim[2] = {4, 4};
   int mpisize, mpirank;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
-  assert(mpisize == dim[0]*dim[1]);
+  assert(mpisize == 4);
+
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
   hid_t file = H5Fopen("data.h5", H5F_ACC_RDONLY, plist);
@@ -21,30 +22,44 @@ int main (int argc, char** argv) {
   hsize_t N[ndim];
   H5Sget_simple_extent_dims(globalspace, N, NULL);
   hsize_t NX = N[0], NY = N[1];
-  hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
-  hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
-  hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
+
+  hsize_t Nlocal[2] = {NX / dim[0], NY / dim[1]};
+  
+  hsize_t start_row = mpirank / 2;
+  hsize_t start_col = mpirank % 2;
+  hsize_t offset[2] = {start_row * Nlocal[0], start_col * Nlocal[1]};
+
+  hsize_t count[2] = {2, 2};
+  hsize_t stride[2] = {2 * Nlocal[0], 2 * Nlocal[1]};
+
+  hsize_t mem_dim[2] = {Nlocal[0] * 2, Nlocal[1] * 2};
+  hid_t localspace = H5Screate_simple(2, mem_dim, NULL);
+
+
   H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
   H5Pclose(plist);
-  vector<int> buffer(Nlocal[0]*Nlocal[1]);
+
+  vector<int> buffer(Nlocal[0] * Nlocal[1] * 4);
   plist = H5Pcreate(H5P_DATASET_XFER);
   H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
+
   auto tic = chrono::steady_clock::now();
   H5Dread(dataset, H5T_NATIVE_INT, localspace, globalspace, plist, &buffer[0]);
   auto toc = chrono::steady_clock::now();
+
   H5Dclose(dataset);
   H5Sclose(localspace);
   H5Sclose(globalspace);
   H5Fclose(file);
   H5Pclose(plist);
+
   double time = chrono::duration<double>(toc - tic).count();
-  int sum = 0;
-  for (int i=0; i<Nlocal[0]*Nlocal[1]; i++)
+  
+  long long sum = 0;
+  for (size_t i = 0; i < buffer.size(); i++)
     sum += buffer[i];
-  printf("sum=%d\n",sum);
-  printf("N=%d: %lf s (%lf GB/s)\n",NX*NY,time,4*NX*NY/time/1e9);
+
+  printf("rank %d sum=%lld\n", mpirank, sum);
+  printf("N=%lld: %lf s (%lf GB/s)\n", (long long)NX*NY, time, 4.0 * Nlocal[0] * Nlocal[1] * 4 * mpisize / time / 1e9);
   MPI_Finalize();
 }
